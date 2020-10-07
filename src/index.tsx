fe88a70a2573912de8c2825c5a5f70288306732a
Nikhil Mehta <nikhil.001mehta@gmail.com>
2020-10-07 18:48:40 +0530

    Using react.strictmode

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import { jsx } from '@emotion/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app';
-
-ReactDOM.render(<App name="nikhil" />, document.getElementById('root'));
 import './styles/index.scss';
+ReactDOM.render(
+  <React.StrictMode>
+    <App name="nikhil" />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
