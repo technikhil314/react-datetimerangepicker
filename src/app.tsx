f1cc68b9b21324cf01fd7d49538acd50c4fa7114
Nikhil Mehta <nikhil.001mehta@gmail.com>
2020-10-07 18:25:46 +0530

    Using emotion for styling

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,35 @@
-import { css } from '@emotion/core';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 import PropTypes from 'prop-types';
 import React from 'react';
-
 export interface AppProps {
   name: string;
 }
 
 export const App: React.FunctionComponent<AppProps> = function () {
   return (
-    <div
-      css={css`
-        padding: 32px;
-        background-color: hotpink;
-        font-size: 24px;
-        border-radius: 4px;
-        &:hover {
-          color: green;
-        }
-      `}
-    >
-      {[1, 2, 3, 4, 5].map((x) => (
-        <h1 key={x}>{x}</h1>
-      ))}
+    <div data-testid="app">
+      <div
+        css={css`
+          padding: 32px;
+          background-color: hotpink;
+          font-size: 24px;
+          border-radius: 4px;
+        `}
+      >
+        {[1, 2, 3, 4, 5].map((x) => (
+          <h1
+            css={css`
+              &:hover {
+                color: var(--demo, #fff);
+              }
+            `}
+            key={x}
+          >
+            {x}
+          </h1>
+        ))}
+      </div>
     </div>
   );
 };
