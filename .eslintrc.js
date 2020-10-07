30ffc5fb06d609d68683ab2553ae2cf757c4b6a3
Nikhil Mehta <nikhil.001mehta@gmail.com>
2020-10-07 18:30:27 +0530

    Emotion related eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,25 +23,31 @@ module.exports = {
     },
   },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jsx-a11y', 'react', 'react-hooks', 'import'],
+  plugins: [
+    '@typescript-eslint',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+    'import',
+    'emotion',
+  ],
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module',
   },
   rules: {
+    // Basic Rules
     indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    'no-unused-vars': 'error',
-    '@typescript-eslint/no-unused-vars': [
+    eqeqeq: ['error'],
+    camelcase: 'error',
+    'no-unused-vars': [
       'error',
       {
-        vars: 'all',
-        args: 'after-used',
-        ignoreRestSiblings: false,
+        varsIgnorePattern: '^jsx$',
       },
     ],
-    '@typescript-eslint/explicit-function-return-type': 'error',
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
     'no-empty': 'error',
     'no-alert': 'error',
     'no-console': [
@@ -50,22 +56,6 @@ module.exports = {
         allow: ['error'],
       },
     ],
-    'react/prop-types': 'error',
-    'react/sort-prop-types': 'error',
-    'import/order': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/no-children-prop': 'error',
-    'react/jsx-no-target-blank': 'error',
-    'react/jsx-key': 'error',
-    'react/jsx-tag-spacing': ['error'],
-    'react/jsx-filename-extension': ['error'],
-    'no-useless-constructor': ['error'],
-    eqeqeq: ['error'],
-    'default-case': 'off',
-    'jsx-a11y/anchor-is-valid': 'error',
-    'jsx-a11y/img-redundant-alt': 'error',
-    'jsx-a11y/alt-text': 'error',
     'no-useless-concat': 'error',
     'no-multiple-empty-lines': [
       'error',
@@ -75,8 +65,42 @@ module.exports = {
         maxBOF: 0,
       },
     ],
-    camelcase: 'error',
-    semi: ['error', 'always'],
+    'linebreak-style': ['error', 'unix'],
+    'default-case': 'off',
+    'no-useless-constructor': ['error'],
+    // typescript rules
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        varsIgnorePattern: '^jsx$',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    // import rules
+    'import/order': 'error',
+    // react rules
+    'react/prop-types': 'error',
+    'react/sort-prop-types': 'error',
+    'react/no-children-prop': 'error',
+    'react/jsx-no-target-blank': 'error',
+    'react/jsx-key': 'error',
+    'react/jsx-tag-spacing': ['error'],
+    'react/jsx-filename-extension': ['error'],
+    // react hooks rules
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    // react jsx a11y rules
+    'jsx-a11y/anchor-is-valid': 'error',
+    'jsx-a11y/img-redundant-alt': 'error',
+    'jsx-a11y/alt-text': 'error',
+    // emotion rules
+    'emotion/jsx-import': 'error',
+    'emotion/no-vanilla': 'error',
+    'emotion/import-from-emotion': 'error',
+    'emotion/styled-import': 'error',
   },
   overrides: [
     {
