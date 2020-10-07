bf7f87bb81007dc4c5c0a8557370cea104238355
Nikhil Mehta <nikhil.001mehta@gmail.com>
2020-10-07 19:00:35 +0530

    Emotion related jest and test case changes

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  snapshotSerializers: ['jest-emotion'],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/testSetup/jestMock.js',
